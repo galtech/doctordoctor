e4c99f364d5be9608aa1b5acef0d832be4a9ddcc
Peter Heylin <peter@peterheylin.com>
2017-05-26 12:03:43 +0100

    20170526 improved page layout

--- app.js
+++ app.js
@@ -5,4 +5,13 @@ angular.module('DoctorDoctorApp', [])
 .controller('DoctorDoctorController', DoctorDoctorController);
 
 
+DoctorDoctorController.$inject = ['$scope'];
+function DoctorDoctorController($scope){
+
+  $scope.showResults = function(){
+      console.log('what are you doing here?');
+  };
+
+}
+
 })();
